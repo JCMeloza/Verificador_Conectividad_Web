39916757ea7ed850c995d07817f5611aed83723e
JuanCarlos <meloza@LAPTOP-VL17EOVM>
2024-09-27 18:37:01 +0200

    modificando y creando solo una funcion

--- conectividad.py
+++ conectividad.py
@@ -6,13 +6,31 @@ modulos tkinter para la interfaz gráfica yurllib'''
 from urllib import request
 from urllib.error import URLError,HTTPError
 import tkinter as tk
+from tkinter import messagebox
 
+import re
 
-def comprobar_conectividad(): 
+#funcion para  comprobar si la url tiene el formato correcto
+def es_url_valida(url):
+
+    url_com= re.compile(r'^(https?://)?(www\.)?[A-Za-z0-9\.-]+(\.[A-Za-z]{2,})+')
+    return bool(url_com.match(url))
 #funcion para comprobar la conectividad y devolver el resultado para mostrarlo en una etiqueta
+def comprobar_conectividad(): 
+
 
     #guardamos el contenido de la caja de texto en la variable
     url=caja_texto.get()
+
+    #verificamos si la url comienza por http:// o https://, y si no lo agregamos
+    if not url.startswith(('http://','https://')):
+        url='http://' + url
+
+    #validamos el formato de url
+    if not es_url_valida(url):
+        messagebox.showerror("Error de URL", "La URL ingresada no es válida. Asegúrate de que tenga el formato correcto (por ejemplo, https://www.ejemplo.com).")
+        resultado_label.config(text="URL no válida. Intente con un formato como http://example.com", fg="red")
+        return
 
     try:
         respuesta=request.urlopen(url) 
@@ -28,41 +46,16 @@ def comprobar_conectividad():
     except URLError as e:
         resultado_label.config(f"URL no válida o no disponible: {e.reason}", fg="red")
     except ValueError:
-        resultado_label.config(text="Por favor introduce una URL válida.", bg="red")
+        resultado_label.config(text="Por favor introduce una URL válida.", fg="red")
 
 
                 
 
     
-'''
-def url_valida(url):
-    #funcion para verificar que el formato de la url es correcto
-    try:
-        resultado=urlparse(url)
-        
-        return all([resultado.scheme,resultado.netloc])
-    except ValueError:
-        return False
 
-def urlTexto():
-    #funcion para guardar en una variable el texto
-    url= caja_texto.get()
-    correcto=url_valida(url)
-    if correcto:
-            codigo=conectividad(url)
-    else:
-         codigo=conectividad(url)
-    print(codigo)
-    return url
-
-def conectividad(url):
-    #funcion para comprobar si la web tiene conectividad
-    conec= urllib.request.urlopen(url)
-    return conec.getcode()
-'''
 #creamos la ventana principal
 ventana= tk.Tk()
-ventana.geometry("550x150") #para poner el tamaño de la ventana
+ventana.geometry("550x250") #para poner el tamaño de la ventana
 ventana.title("Comprobador de conectividad")
 
 #creamos la etiqueta a la izquierda de la caja de texto
